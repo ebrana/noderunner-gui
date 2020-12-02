d464d17e59b861d17be938ebd078aeba682925c0
Martin Chudoba <martin.chudoba@seznam.cz>
2020-12-02 15:53:54 +0100

    prevedeni store na typescript

--- src/store/store.ts
+++ src/store/store.ts
@@ -18,6 +18,7 @@ export default createStore({
         waitingCounter: 0,
         plannedCounter: 0,
         threadsCounter: 0,
+        threads: [],
         threadsStats: [],
         runningJobsList: [],
         newThreadsStat: [],
@@ -27,66 +28,72 @@ export default createStore({
     },
     mutations: {
         showPreloader(state, value) {
-            state.showPreloader = value;
+            state.showPreloader = value
         },
         server(state, hostname) {
-            state.server = hostname;
+            state.server = hostname
         },
         invalidateChart(state, value) {
-            state.invalidateChart = value;
+            state.invalidateChart = value
         },
         colors(state, value) {
-            state.colors = value;
+            state.colors = value
         },
         emitData(state, data) {
-            let findIndex = false;
-            let dataProvider;
+            let findIndex = false
+            let dataProvider
 
             // console.log(config.events[data.key].event, data.value)
             switch (config.events[data.key].event) {
                 case 'historyCountDecreased':
                     if (state.historyCounterInit === true) {
-                        state.historyCounter -= data.value;
+                        state.historyCounter -= data.value
                         if (state.historyCounter < 0) {
-                            state.historyCounter = 0;
+                            state.historyCounter = 0
                         }
                     }
                     break;
                 case 'historyCountIncreased':
                     if (state.historyCounterInit === true) {
-                        state.historyCounter += data.value;
+                        state.historyCounter += data.value
                     }
                     break;
                 case 'historyCount':
-                    state.historyCounter = data.value;
-                    state.historyCounterInit = true;
+                    state.historyCounter = data.value
+                    state.historyCounterInit = true
                     break;
                 case 'threadsCount':
-                    state.threadsCounter = data.value;
+                    // state.threadsCounter = data.value;
+                    // state.showPreloader = false;
+                    break;
+                case 'threadsSettings':
+                case 'settingSaved':
+                    state.threadsCounter = data.value.length
+                    state.threads = data.value
                     state.showPreloader = false;
                     break;
                 case 'plannedCount':
-                    state.plannedCounter = data.value;
+                    state.plannedCounter = data.value
                     break;
                 case 'waitingCount':
-                    state.waitingCounter = data.value;
-                    state.waitingCounterInit = true;
+                    state.waitingCounter = data.value
+                    state.waitingCounterInit = true
                     break;
                 case 'waitingCountIncreased':
                     if (state.waitingCounterInit === true) {
-                        state.waitingCounter += data.value;
+                        state.waitingCounter += data.value
                     }
                     break;
                 case 'waitingCountDecreased':
                     if (state.waitingCounterInit === true) {
-                        state.waitingCounter -= data.value;
+                        state.waitingCounter -= data.value
                         if (state.waitingCounter < 0) {
-                            state.waitingCounter = 0;
+                            state.waitingCounter = 0
                         }
                     }
                     break;
                 case 'runningJobsList':
-                    state.runningJobsList = data.value;
+                    state.runningJobsList = data.value
                     if (Object.keys(state.runningJobsList).length === 0) { // min lenght 1
                         // @ts-ignore
                         state.runningJobsList.push({'thread': 0, 'command': ''})
@@ -98,14 +105,14 @@ export default createStore({
                         // @ts-ignore
                         if (state.runningJobsList[index].thread == data.value.thread) {
                             // @ts-ignore
-                            state.runningJobsList[index] = data.value;
-                            findIndex = true;
+                            state.runningJobsList[index] = data.value
+                            findIndex = true
                             break;
                         }
                     }
                     if (findIndex === false) {
                         // @ts-ignore
-                        state.runningJobsList.push(data.value);
+                        state.runningJobsList.push(data.value)
                     }
                     break;
                 case 'jobCompleted':
@@ -125,68 +132,74 @@ export default createStore({
                         for (const i in d.byThread) {
                             Object.assign(d, {['thread'+(i)]: d.byThread[i]})
                         }
-                        return d;
+                        return d
                     });
                     break;
                 case 'newThreadsStat':
                     dataProvider = data.value;
                     dataProvider.date = new Date(dataProvider.intervalTo*1000)
                     for (const i in dataProvider.byThread) {
-                        dataProvider['thread'+(i)] = dataProvider.byThread[i];
+                        dataProvider['thread'+(i)] = dataProvider.byThread[i]
                     }
                     // console.log(dataProvider)
-                    state.newThreadsStat = dataProvider;
+                    state.newThreadsStat = dataProvider
+                    break;
+                case 'settingSavedFalse':
+                    state.showPreloader = false;
                     break;
             }
         }
     },
     actions: {
         emitData(context, data) {
-            context.commit('emitData', data);
+            context.commit('emitData', data)
         },
         server(context, hostname) {
-            context.commit('server', hostname);
+            context.commit('server', hostname)
         },
         invalidateChart(context, value) {
-            context.commit('invalidateChart', value);
+            context.commit('invalidateChart', value)
         },
         colors(context, value) {
-            context.commit('colors', value);
+            context.commit('colors', value)
         },
         showPreloader(context, value) {
-            context.commit('showPreloader', value);
+            context.commit('showPreloader', value)
         }
     },
     getters: {
         historyCount: state => {
-            return state.historyCounter;
+            return state.historyCounter
         },
         threadsCount: state => {
-            return state.threadsCounter;
+            return state.threadsCounter
+        },
+        threads: state => {
+            return state.threads
         },
         plannedCount: state => {
-            return state.plannedCounter;
+            return state.plannedCounter
         },
         waitingCount: state => {
-            return state.waitingCounter;
+            return state.waitingCounter
         },
         runningJobsList: state => {
-            return state.runningJobsList;
+            return state.runningJobsList
         },
         threadsStats: state => {
-            return state.threadsStats;
+            return state.threadsStats
         },
         newThreadsStat: state => {
-            return state.newThreadsStat;
+            return state.newThreadsStat
         },
         invalidateChart: state => {
-            return state.invalidateChart;
+            return state.invalidateChart
         },
         colors: state => {
-            return state.colors;
+            return state.colors
         },
         showPreloader: state => {
-            return state.showPreloader;
+            return state.showPreloader
         }
     }
 });
